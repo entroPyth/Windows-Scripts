4b9c45bb4b28c709d7037e3513d936cd7c9a80a2
Jonathon Robin <jar03j@outlook.com>
2020-05-22 23:11:04 -0400

    Add file exist check

--- Get-SpotlightWallpaper/Get-SpotlightWallpaper.ps1
+++ Get-SpotlightWallpaper/Get-SpotlightWallpaper.ps1
@@ -1,11 +1,13 @@
+# 05-22-2020 entropyth
 # Get Spotlight Wallpapers with dimensions 1920X1080
+# Copy to $env:USERPROFILE\Pictures\Wallpaper\Spotlight\ and add ".jpg" file extension.
 
 # Get List of Spotlight files
 $spotlightFolder = "$env:USERPROFILE\AppData\Local\Packages\Microsoft.Windows.ContentDeliveryManager_cw5n1h2txyewy\LocalState\Assets\"
 $spotlightFiles = (Get-ChildItem $spotlightFolder).Name
-$wallpaperPath = "$env:USERPROFILE\Pictures\Wallpaper\"
+$wallpaperPath = "$env:USERPROFILE\Pictures\Wallpaper\Spotlight\"
 
-# Create Wallpaper Folder in $env:USERPROFILE\Pictures if it doesn't exist
+# Create Wallpaper Folder in $env:USERPROFILE\Pictures\Spotlight\ if it doesn't exist
 If (!(Test-Path -Path $wallpaperPath)) {
     New-Item -ItemType Directory -Force -Path $wallpaperPath
 }
@@ -16,9 +18,18 @@ ForEach ($file in $spotlightFiles) {
     add-type -AssemblyName System.Drawing
     $image = New-Object System.Drawing.Bitmap $spotlightFolder$file
     If ($image.Height -eq '1080' -AND $image.Width -eq '1920') {
-        Write-Host "Found $file"
-        Copy-Item $spotlightFolder$File $wallpaperPath
-        Rename-Item -Path $wallpaperPath$file -NewName ($file + ".jpg")
+        Write-Host "Found 1920X1080 file: $file"
+        If (!(Test-Path -Path "$wallpaperPath$file.jpg")) {
+            Write-Host "Copying $file to $wallpaperPath"
+            Copy-Item $spotlightFolder$File "$wallpaperPath$file.jpg"
+        } 
+        elseif (Test-Path -Path "$wallpaperPath$file.jpg") {
+                Write-Host "$file already exists, skipping"
+        }
     }
 }
 
+## Set Desktop Background to Slideshow with path to $wallpaperPath
+#        If (Test-Path -Path ($wallpaperPath$file + ".jpg") {
+#            Write-Host "$file already exists, skipping"
+#        } else
